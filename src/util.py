4c33730300862e029b0c34ab53b9ac7fc6a7b92f
Juho Turunen <juho.turunen89@gmail.com>
2024-11-24 15:59:03 +0200

    Fixed bug with created_at date not being shown correctly

--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
 from entities.citation import Article
 import os.path
 
+
 class UserInputError(Exception):
     pass
 
@@ -9,37 +10,37 @@ def citation_data_to_class(form):
     result = None
     if form.get("type") == "article":
         result = Article(
-            key=form.get("key"), # Required
-            author=form.get("author"), # Required
-            title=form.get("title"), # Required
-            journal=form.get("journal"), # Required
-            year=form.get("year"), # Required
+            key=form.get("key"),  # Required
+            author=form.get("author"),  # Required
+            title=form.get("title"),  # Required
+            journal=form.get("journal"),  # Required
+            year=form.get("year"),  # Required
+            created_at=form.get("created_at"),
             volume=form.get("volume"),
             number=form.get("number"),
             pages=form.get("pages"),
             month=form.get("month"),
-            note=form.get("note")
+            note=form.get("note"),
         )
 
     return result
 
 
 def citation_data_to_bibtex_file(list):
-    curly_brace_open="{"
-    curly_brace_close="}"
-    banned_keys={"created_at","type","key"}
-    file_path=os.path.join(f"{os.path.dirname(__file__)}/bibtex_files/","citations.bib")
-    with open(file_path,"w") as bibtex:
+    curly_brace_open = "{"
+    curly_brace_close = "}"
+    banned_keys = {"created_at", "type", "key"}
+    file_path = os.path.join(f"{os.path.dirname(__file__)}/bibtex_files/", "citations.bib")
+    with open(file_path, "w") as bibtex:
         for form_as_class in list:
-            form=vars(form_as_class)
-            type=form.get("type")
-            formkey=form.get("key")
-            tab="\t"
+            form = vars(form_as_class)
+            type = form.get("type")
+            formkey = form.get("key")
+            tab = "\t"
             bibtex.write(f"@{type}")
             bibtex.write(curly_brace_open)
             bibtex.write(f"{formkey},\n")
-            for key,value in form.items():
+            for key, value in form.items():
                 if key not in banned_keys and value:
                     bibtex.write(f"{tab}{key} = {{{value}}},\n")
-            bibtex.write(curly_brace_close+"\n")
-    
+            bibtex.write(curly_brace_close + "\n")
