edf76cd8a5619752f16dde761a814a3182ea80ca
Juho Turunen <juho.turunen89@gmail.com>
2024-11-26 19:49:05 +0200

    Added growing input fields

--- src/static/javascript/add_citation.js
+++ src/static/javascript/add_citation.js
@@ -1,3 +1,20 @@
+const form = document.getElementById("citation_form");
+let input_fields = form.getElementsByTagName("textarea")
+
+window.addEventListener('load', function () {
+    Array.prototype.forEach.call(input_fields, input_field => {
+        input_field.style.height = input_field.scrollHeight + 'px';
+    });
+});
+
+Array.prototype.forEach.call(input_fields, input_field => {
+    input_field.addEventListener('input', function () {
+        this.style.height = 'auto';
+        this.style.height = this.scrollHeight + 'px';
+    });
+});
+
+
 let opt_btn = document.getElementById('opt_btn');
 opt_btn.addEventListener("click", opt_field_revealer);
 
