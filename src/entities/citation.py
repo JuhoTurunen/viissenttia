6b1eb02d6e616a1eb20ce6e1b922f657188dc799
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2024-12-02 13:50:41 +0200

    Added class Book to be used for book type citations

--- src/entities/citation.py
+++ src/entities/citation.py
@@ -44,6 +44,45 @@ class Article(CitationBase):
             f"note={self.note}, created_at={self.created_at})"
         )
 
+
+class Book(CitationBase):
+    def __init__(
+        self,
+        key,
+        author,
+        title,
+        year,
+        publisher,
+        created_at=None,
+        volume=None,
+        series=None,
+        address=None,
+        edition=None,
+        note=None,
+    ):
+        # Call CitationBase class __init__
+        super().__init__(key=key, citation_type="book", created_at=created_at)
+
+        # Initialize specific attributes for the Book class
+        self.author = author
+        self.title = title
+        self.year = year
+        self.publisher = publisher
+        self.volume = volume
+        self.series = series
+        self.address = address
+        self.edition = edition
+        self.note = note
+
+    def __str__(self):
+        return (
+            f"Book(key={self.key}, author={self.author}, title={self.title}, "
+            f"year={self.year}, publisher={self.publisher}, volume={self.volume}, "
+            f"series={self.series}, address={self.address}, edition={self.edition}, "
+            f"note={self.note}, created_at={self.created_at})"
+        )
+
+
 class Inproceedings(CitationBase):
     def __init__():
         pass
