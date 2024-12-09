7616e4835b9e4d2688584789a0f563fb9986f7af
Juho Turunen <juho.turunen89@gmail.com>
2024-12-09 02:06:38 +0200

    Fixed bug with multiple citation popups showing at once

--- src/static/javascript/index.js
+++ src/static/javascript/index.js
@@ -1,15 +1,19 @@
 let citations = document.getElementsByClassName('citation');
-
+let citation_open = false;
 
 Array.prototype.forEach.call(citations, citation => {
     let citation_brief = citation.getElementsByClassName("citation_brief")[0];
     let popup = citation.getElementsByClassName("popup")[0];
     citation_brief.addEventListener("click", () => {
-        popup.classList.add("shown");
+        if (!citation_open) {
+            citation_open = true;
+            popup.classList.add("shown");
+        }
     });
     let close_btn = popup.getElementsByTagName("button")[0];
 
     close_btn.addEventListener("click", () => {
+        citation_open = false;
         popup.classList.remove("shown");
     });
 });
